3613b10e1c768c78a48bded7bcea01dbcc742784
SimonLembeye <github-Sim1997>
2020-09-15 14:47:57 +0200

    chg: mv static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,7 @@ environ.Env.read_env()
 SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["0.0.0.0", "127.0.0.1", "localhost", "enertrack.herokuapp.com"]
 
