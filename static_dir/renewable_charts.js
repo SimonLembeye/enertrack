2fef6b2f9dd4be395a71bebb4609b586cfc2c025
SimonLembeye <github-Sim1997>
2020-09-17 22:21:27 +0200

    new: 17/09

--- static_dir/renewable_charts.js
+++ static_dir/renewable_charts.js
@@ -25,8 +25,42 @@ function renderChart(divId, data, labels) {
     });
 }
 
+function renderProductionChart(data) {
+    console.log('YES')
+    let ctx = document.getElementById("productionChart").getContext('2d');
+    var myBarChart = new Chart(ctx, {
+        type: 'horizontalBar',
+        data: {
+            labels: ["Solaire", "Eolien"],
+            datasets: [{
+                backgroundColor: ["rgba(255, 192, 0, 0.2)", "rgba(0, 192, 255, 0.2)"],
+                data: [50.56, 34.32],
+            },{
+                backgroundColor: ["#ffc107", "#5b80b2"],
+                data: [20, 10],
+            }],
+        },
+        options: {
+            legend: { display: false },
+            scales: {
+                xAxes: [{
+                    ticks: {
+                        min: 0
+                    },
+                    staked: false
+                }],
+                yAxes: [{
+                    stacked: true
+                }]
+            }
+        }
+    });
+}
+
+
 $(document).ready(
     function () {
+        renderProductionChart("DD");
         let data = JSON.parse(chart_data);
         let todayData = data.today_data;
         renderChart("todayChart", todayData.values, todayData.labels);
